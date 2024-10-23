1fa9519d2505d00faf26c22a0395112d6343d8d9
Aleksei_Ushakov <argarintem@gmail.com>
2024-10-23 23:28:37 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsMap = Object.fromEntries(bracketsConfig);
+  let sameBrackets = new Set(
+    bracketsConfig.filter(([open, close]) => open === close).map(([open]) => open)
+  );
+
+  for (let char of str) {
+    if (sameBrackets.has(char)) {
+      if (stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } 
+    else if (bracketsMap[char]) {
+      stack.push(char);
+    } 
+    else {
+      if (stack.length === 0 || bracketsMap[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
